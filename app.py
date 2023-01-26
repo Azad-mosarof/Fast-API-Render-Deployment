6637493abfb125f35a19395a8f144511dd94e1b9
Azad-mosarof <misarofazad@gmail.com>
2023-01-26 20:22:59 +0300

    update

--- app.py
+++ app.py
@@ -20,20 +20,20 @@ model = pickle.load(open('model/expense_model.pkl', 'rb'))
 def main():
     return {'message': 'Welcome to Render Fast Api'}
 
-@app.post('/prediction')
-def getPrediction(input_parameters : model_input):
-    input_data = input_parameters.json()
-    input_dictionary = json.loads(input_data)
+# @app.post('/prediction')
+# def getPrediction(input_parameters : model_input):
+#     input_data = input_parameters.json()
+#     input_dictionary = json.loads(input_data)
 
-    age = int(input_dictionary['age'])
-    bmi = int(input_dictionary['bmi'])
-    children = int(input_dictionary['children'])
-    sex = int(input_dictionary['Sex'])
-    smoker = int(input_dictionary['Smoker'])
-    region = int(input_dictionary['Region'])
+#     age = int(input_dictionary['age'])
+#     bmi = int(input_dictionary['bmi'])
+#     children = int(input_dictionary['children'])
+#     sex = int(input_dictionary['Sex'])
+#     smoker = int(input_dictionary['Smoker'])
+#     region = int(input_dictionary['Region'])
 
-    #get prediction
-    input_cols = [[age, bmi, children, sex, smoker, region]]
-    prediction = model.predict(input_cols)
-    output = round(prediction[0], 2)
-    return "Your predicted annual healthcare expense is ${}".format(output)
+#     #get prediction
+#     input_cols = [[age, bmi, children, sex, smoker, region]]
+#     prediction = model.predict(input_cols)
+#     output = round(prediction[0], 2)
+#     return "Your predicted annual healthcare expense is ${}".format(output)
